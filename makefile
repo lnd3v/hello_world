96f8a608b5021c62e852ad4511fcd6f686adc00f
lndev <lnd3v@proton.me>
2023-07-19 02:45:50 +0000

    Added support for ECDSA recoverable signature and updated makefile to remove the target executable before building

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all:
-	rm target/debug/hello_world
+	rm -f target/debug/hello_world
 	cargo build
 	target/debug/hello_world	
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ fn main() {
     let full = Secp256k1::new();
     let (sk, pk) = full.generate_keypair(&mut rand::thread_rng());
     let msg = Message::from_slice(&msg_hash).unwrap();
-    let sig = full.sign_ecdsa(&msg, &sk);
+    let sig = full.sign_ecdsa_recoverable(&msg, &sk);
+    let (recovery_id, sig_data) = sig.serialize_compact();
+    let mut signature = vec![31 + recovery_id.to_i32() as u8];
+    signature.extend(sig_data.iter());
     let zbase32_encoded = zbase32::encode_full_bytes(message.as_bytes());
     println!("Signature: {:?}", zbase32_encoded);
 }
