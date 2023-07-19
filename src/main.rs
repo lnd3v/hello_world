18d998ee0ba54e1cdca57cbea22b77c23cdacebd
lndev <lnd3v@proton.me>
2023-07-19 02:52:03 +0000

    Added a separate function to handle message signing and simplified the main function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-#[allow(unused_variables)]
 use sha2::{Sha256, Digest};
 use zbase32;
 use secp256k1::{Secp256k1, Message};
 use secp256k1::rand;
 
-#[allow(unused_variables)]
+fn sign_message(message: &str) -> String {
+    let message_hash = Sha256::digest(message.as_bytes());
+
+    let secp = Secp256k1::new();
+    let (secret_key, _public_key) = secp.generate_keypair(&mut rand::thread_rng());
+
+    let msg = Message::from_slice(&message_hash).unwrap();
+    let signature = secp.sign_ecdsa_recoverable(&msg, &secret_key);
+
+    let (recovery_id, signature_data) = signature.serialize_compact();
+    let mut complete_signature = vec![31 + recovery_id.to_i32() as u8];
+    complete_signature.extend_from_slice(&signature_data);
+
+    let zbase32_encoded_message = zbase32::encode_full_bytes(message.as_bytes());
+
+    zbase32_encoded_message
+}
+
 fn main() {
     let message = "Lighting Signed Message:Hello, World!";
-    let msg_hash = Sha256::digest(&message.as_bytes());
+    let signature = sign_message(message);
 
-    let full = Secp256k1::new();
-    let (sk, pk) = full.generate_keypair(&mut rand::thread_rng());
-    let msg = Message::from_slice(&msg_hash).unwrap();
-    let sig = full.sign_ecdsa_recoverable(&msg, &sk);
-    let (recovery_id, sig_data) = sig.serialize_compact();
-    let mut signature = vec![31 + recovery_id.to_i32() as u8];
-    signature.extend(sig_data.iter());
-    let zbase32_encoded = zbase32::encode_full_bytes(message.as_bytes());
-    println!("Signature: {:?}", zbase32_encoded);
+    println!("Signature: {:?}", signature);
 }
